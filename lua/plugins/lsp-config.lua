4a524a60a6eda72366d4d0c85ab02e0a4ac7b818
Arif Hossain <hamid_kawser@hotmail.be>
2024-09-23 20:40:47 +0600

    Added elixir-ls lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,13 +20,29 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.ruby_lsp.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        on_attach = on_attach
       })
       lspconfig.emmet_language_server.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        on_attach = on_attach
       })
       lspconfig.dockerls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        on_attach = on_attach
+      })
+      lspconfig.elixirls.setup({
+        cmd = { "elixir-ls"},
+        capabilities = capabilities,
+        on_attach = on_attach
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,10 +4,10 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
         null_ls.builtins.formatting.erb_format,
         null_ls.builtins.formatting.htmlbeautifier,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
