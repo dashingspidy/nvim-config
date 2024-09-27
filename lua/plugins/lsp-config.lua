de47d8c89dccdcd08d69e4fcea68cfd5fb1fe6ae
Arif Hossain <hamid_kawser@hotmail.be>
2024-09-27 11:32:48 +0600

    Added evil lualine

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -35,10 +35,6 @@ return {
         capabilities = capabilities,
         on_attach = on_attach
       })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach
-      })
       lspconfig.elixirls.setup({
         cmd = { "elixir-ls"},
         capabilities = capabilities,
